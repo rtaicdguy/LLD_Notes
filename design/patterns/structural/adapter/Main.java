30b2c3e865efd89cfeb32d6ff3fe3e2ecce1a3e7
rishabhtripathi <rishabhtripathi123asdf@gmail.com>
2025-12-18 21:34:28 +0530

    initial commit

---
+++ design/patterns/structural/adapter/Main.java
@@ -0,0 +1,22 @@
+package design.patterns.structural.adapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Student> students=new ArrayList<>();
+        CollegeStudent collegeStudent=new CollegeStudent("loda","lasan","ll@gmail.com");
+        SchoolStudent schoolStudent=new SchoolStudent("dhawal","jain","abc@gmail.com");
+
+        students.add(collegeStudent);
+        //students.add(schoolStudent);
+
+        students.add(new SchoolStudentAdapter(schoolStudent));
+
+        for(Student s: students){
+            System.out.println(s.toString());
+        }
+
+    }
+}
---
+++ design/patterns/structural/adapter/Student.java
@@ -0,0 +1,8 @@
+package design.patterns.structural.adapter;
+
+public interface Student {
+    public String getName();
+    public String getSurname();
+    public String getEmail();
+
+}
